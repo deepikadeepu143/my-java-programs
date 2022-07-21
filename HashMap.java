75b1067de75d51808e5ddb7e3022771864496261
deepikadeepu143 <108505880+deepikadeepu143@users.noreply.github.com>
2022-07-21 21:06:48 +0530

    Adding another java file

---
+++ HashMap.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class HashMap
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int a,b,c,i;
+		for(i=0;i<3;i++)
+		{
+			System.out.print("Enter the a and b values: ");
+			b=sc.nextInt();
+			c=sc.nextInt();
+		}
+	}
+}
