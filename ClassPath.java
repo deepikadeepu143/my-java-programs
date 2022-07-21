009d5b5ecf260eb131b1f3bc480893615d71a35d
deepikadeepu143 <108505880+deepikadeepu143@users.noreply.github.com>
2022-07-21 10:23:51 +0530

    Adding another file

---
+++ ClassPath.java
@@ -0,0 +1,19 @@
+// Java Program to Illustrate Usage of importing
+// Classes from packages and sub-packages
+  
+// Here we are importing all classes from
+// java.io (input-output package)
+import java.io.*;
+  
+// Main class
+class ClassPath
+{
+  
+    // Main driver method
+    public static void main(String[] args)
+    {
+  
+        // Print statement
+        System.out.println("I/O classes are imported from java.io package");
+    }
+}
