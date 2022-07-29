2a61bedea205aa2995d0bf42aeb1c3a88aab64eb
deepikadeepu143 <108505880+deepikadeepu143@users.noreply.github.com>
2022-07-29 20:23:04 +0530

    adding another java file

---
+++ Name1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Name1
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your firstname");
+String  fname=sc.next();
+System.out.println("enter your lastname");
+String lname=sc.next();
+System.out.println("Your name is "+fname+" "+lname);
+}
+}
